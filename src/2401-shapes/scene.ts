1799c61540e4e294fc15c1fa1cc60387885edf09
Michelle Enos <32843306+crankysparrow@users.noreply.github.com>
2024-01-16 09:36:42 -0500

    models!

--- src/2401-shapes/scene.ts
+++ src/2401-shapes/scene.ts
@@ -12,7 +12,7 @@ const world = new World(sizes)
 const clock = new THREE.Clock()
 const gui = new GUI()
 
-world.camera.position.set(5, 3, 4)
+world.camera.position.set(0, 6, 4)
 world.renderer.outputColorSpace = THREE.SRGBColorSpace
 world.controls.zoomSpeed = 0.5
 
@@ -27,19 +27,41 @@ const gltfLoader = new GLTFLoader()
  * Textures & Materials
  */
 
-const bakedTexture = textureLoader.load('./bake.jpg')
+const bakedTexture = textureLoader.load('./bakehouse2.jpg')
 bakedTexture.flipY = false
 bakedTexture.colorSpace = THREE.SRGBColorSpace
 const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture })
 
+const lampMaterial = new THREE.MeshBasicMaterial({
+   color: '#ffe7db',
+})
+const windowMaterial = new THREE.MeshBasicMaterial({
+   color: '#fff3d6',
+})
+
 /**
  * Model
  */
-gltfLoader.load('./shapes.glb', (gltf) => {
+gltfLoader.load('./house2.glb', (gltf) => {
    console.log(gltf)
+
+   const children = gltf.scene.children
+   const emissionWindow = children.find((child) => child.name === 'emissionWindow')
+   const emissionRoundWindow = children.find((child) => child.name === 'emissionRoundWindow')
+   const emissionLamp = children.find((child) => child.name === 'emissionLamp')
    gltf.scene.traverse((child) => {
       child instanceof THREE.Mesh && (child.material = bakedMaterial)
    })
+
+   if (emissionLamp instanceof THREE.Mesh) {
+      emissionLamp.material = lampMaterial
+   }
+   if (emissionWindow instanceof THREE.Mesh) {
+      emissionWindow.material = windowMaterial
+   }
+   if (emissionRoundWindow instanceof THREE.Mesh) {
+      emissionRoundWindow.material = windowMaterial
+   }
 
    world.scene.add(gltf.scene)
 })
@@ -50,11 +72,19 @@ gltfLoader.load('./shapes.glb', (gltf) => {
 
 const debg = {
    clearColor: '#ffffff',
+   windowColor: windowMaterial.color.getHexString(),
+   lampColor: lampMaterial.color.getHexString(),
 }
 world.renderer.setClearColor(debg.clearColor)
 
 gui.addColor(debg, 'clearColor').onChange((val: string) => {
    world.renderer.setClearColor(val)
+})
+gui.addColor(debg, 'windowColor').onChange((val: string) => {
+   windowMaterial.color.set(val)
+})
+gui.addColor(debg, 'lampColor').onChange((val: string) => {
+   lampMaterial.color.set(val)
 })
 
 const animate = () => {
